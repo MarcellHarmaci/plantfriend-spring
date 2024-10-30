8d6b19b8255759d53e8b15070004d2ac1c5dd84c
Marcell Harmaci <harmaci.marci@gmail.com>
2024-10-30 22:50:08 +0200

    Simplify plant data model with lombok

--- src/main/java/com/harmaci/plantfriend/repository/model/Plant.java
+++ src/main/java/com/harmaci/plantfriend/repository/model/Plant.java
@@ -1,12 +1,10 @@
 package com.harmaci.plantfriend.repository.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-
-import java.util.Objects;
+import lombok.Data;
 
 @Entity
+@Data
 public class Plant {
 
     @Id
@@ -16,15 +14,9 @@ public class Plant {
 
     private String name;
 
-    /**
-     * Default constructor for Interceptor
-     */
     public Plant() {
     }
 
-    /**
-     * Constructor for a Habit without an id
-     */
     public Plant(String name) {
         this.name = name;
     }
@@ -34,66 +26,6 @@ public class Plant {
         this.name = name;
     }
 
-    public Plant id(Long id) {
-        this.id = id;
-        return this;
-    }
-
-    @NotNull
-    @JsonProperty("id")
-    public Long getId() {
-        return id;
-    }
-
-    public Plant name(String name) {
-        this.name = name;
-        return this;
-    }
-
-    @NotNull
-    @JsonProperty("name")
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Plant plant)) {
-            return false;
-        }
-
-        return Objects.equals(this.id, plant.id) &&
-                Objects.equals(this.name, plant.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("class Habit {\n");
-        sb.append("    id: ").append(toIndentedString(id)).append("\n");
-        sb.append("    name: ").append(toIndentedString(name)).append("\n");
-        sb.append("}");
-        return sb.toString();
-    }
-
-    /**
-     * Convert the given object to string with each line indented by 4 spaces
-     * (except the first line).
-     */
-    private String toIndentedString(Object o) {
-        if (o == null) {
-            return "null";
-        }
-        return o.toString().replace("\n", "\n    ");
-    }
 }
 
 

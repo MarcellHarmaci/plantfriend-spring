7b7a555a6739c33adf4d3afc7a808486ac64ab32
Marcell Harmaci <harmaci.marci@gmail.com>
2025-04-04 09:38:23 +0300

    Add custom jackson config - fix JsonOptional and date/time encodings

---
+++ src/main/java/com/harmaci/plantfriend/config/JacksonConfig.java
@@ -0,0 +1,21 @@
+package com.harmaci.plantfriend.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.openapitools.jackson.nullable.JsonNullableModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JsonNullableModule());
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return objectMapper;
+    }
+}
